b6e55ee581237786265458e08262db1b466dcb02
unknown <pedro.coding.contact@gmail.com>
2020-12-10 16:13:27 -0300

    added drawer for smaller screens

--- src/Components/Ui/Header.js
+++ src/Components/Ui/Header.js
@@ -10,6 +10,10 @@ import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import {useTheme} from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 
 function ElevationScroll(props) {
@@ -28,12 +32,12 @@ function ElevationScroll(props) {
   const useStyles = makeStyles(theme =>({
    toolbarMargin: {
      ...theme.mixins.toolbar,
-     marginBottom: "3.3em",
+     marginBottom: "2.3em",
      [theme.breakpoints.down('md')]:{
-      marginBottom:"2.3em"
+      marginBottom:"1.5em"
     },
     [theme.breakpoints.down('xs')]: {
-      marginBottom:"1.5em"
+      marginBottom:"0.7em"
     }
    },
    logoContainer:{
@@ -42,12 +46,12 @@ function ElevationScroll(props) {
     }
    },
    Logo:{
-     height:"8em",
+     height:"7em",
      [theme.breakpoints.down('md')]:{
-       height:"7em"
+       height:"6em"
      },
      [theme.breakpoints.down('xs')]: {
-       height:"5.5em"
+       height:"4.5em"
      }
    },
    tabContainer:{
@@ -58,16 +62,29 @@ function ElevationScroll(props) {
      ...theme.typography.tab,
      minWidth: "10px",
      marginLeft: "25px"
+   },
+   drawerIcon:{
+    height:"50px",
+    width:"50px"
+  },
+   drawerIconContainer:{
+     marginLeft:"auto",
+     "&:hover":{
+       backgroundColor:"transparent"
+     }     
    }
   })) 
 
 export default function Header (props) {
   const classes = useStyles()
   const theme = useTheme();
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
   const matches = useMediaQuery(theme.breakpoints.down('md'))
+
+  const [openDrawer, setOpenDrawer] = useState(false)
   const [value,setValue] = useState(0);
-  const changeHandler = (e, value) =>{
-    setValue(value)
+  const changeHandler = (e, newValue) =>{
+    setValue(newValue)
   }
   useEffect(()=>{
     if(window.location.pathname === "/" && value !== 0){
@@ -89,6 +106,17 @@ export default function Header (props) {
     </React.Fragment>
   )
 
+  const drawer = (
+    <React.Fragment>
+      <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer} onClose={()=> setOpenDrawer(false)} onOpen={()=>setOpenDrawer(true)}> 
+      Example Drawer
+      </SwipeableDrawer>
+      <IconButton className={classes.drawerIconContainer} onClick={()=> setOpenDrawer(!openDrawer)} disableRipple>
+        <MenuIcon className={classes.drawerIcon}/>
+      </IconButton>
+    </React.Fragment>
+  )
+
    return(
        <React.Fragment>
        <ElevationScroll>
@@ -97,7 +125,7 @@ export default function Header (props) {
             <Button component={Link} to="/" onClick={()=> setValue(0)} disableRipple className={classes.logoContainer}>
               <img src={Logo} alt="Logo" className={classes.Logo}/>
             </Button>
-              {matches ? null : tabs}          
+              {matches ? drawer : tabs}          
             </Toolbar>
         </AppBar>
        </ElevationScroll>
